959664835677f17acb748f7f5720b97e73749fd0
unknown <omkar.shinde@sjinnovation.com>
2026-04-14 12:06:44 +0530

    [FEAT] Align admin task streams UI with category-based stream management

--- src/modules/actions/hooks/index.ts
+++ src/modules/actions/hooks/index.ts
@@ -2,3 +2,16 @@ export { useTasksV2, useTask, useTaskBySlug, useTaskStats, useTodayTasks, useThi
 export { useTaskStreams, useTaskStream, useTaskStreamBySlug, useCreateStream, useUpdateStream, useArchiveStream } from "./useTaskStreams";
 export { useTaskComments, useAddComment, useUpdateComment, useDeleteComment } from "./useTaskComments";
 export { useTaskViewPreference } from "./useTaskViewPreference";
+export {
+  useTaskCategories,
+  useCreateTaskCategory,
+  useUpdateTaskCategory,
+  useDeleteTaskCategory,
+  useToggleTaskCategory,
+} from "./useTaskCategories";
+export {
+  useTaskCategoryAccess,
+  useAddTaskCategoryAccess,
+  useRemoveTaskCategoryAccess,
+} from "./useTaskCategoryAccess";
+export { useStreamTaskCounts } from "./useStreamTaskCounts";
